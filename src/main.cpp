4bc6e95e32ffb1862c64a239e9dea96b90f275d4
Rayival Dede Saputra <dederayival19@gmail.com>
2026-05-28 10:28:10 +0700

    Menambahkan struktur menu utama

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,40 @@ using namespace std;
 int main() {
     int pilihan;
 
-    cout << "===================================" << endl;
-    cout << " SISTEM MANAJEMEN PERPUSTAKAAN " << endl;
-    cout << "===================================" << endl;
+    do {
+        system("cls");
 
-    cout << "1. Login" << endl;
-    cout << "2. Keluar" << endl;
-    cout << "Pilih Menu : ";
-    cin >> pilihan;
+        cout << "===================================" << endl;
+        cout << " SISTEM MANAJEMEN PERPUSTAKAAN " << endl;
+        cout << "===================================" << endl;
+
+        cout << "1. Login" << endl;
+        cout << "2. Keluar" << endl;
+        cout << endl;
+
+        cout << "Pilih menu : ";
+        cin >> pilihan;
+
+        switch(pilihan) {
+            case 1:
+                cout << "\n[ MENU LOGIN ]" << endl;
+
+                // di isi login disini sama Ridzki
+
+                system("pause");
+                break;
+
+            case 2:
+                cout << "\nTerima kasih..." << endl;
+                break;
+
+            default:
+                cout << "\nMenu tidak tersedia!" << endl;
+                system("pause");
+                break;
+        }
+
+    } while(pilihan != 2);
 
     return 0;
 }
